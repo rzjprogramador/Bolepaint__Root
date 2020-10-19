18623d5cde36b747e048d4d31445cbe97553cbc4
rzjprogramador <rzjprogramador@gmail.com>
2020-10-19 13:55:49 -0300

    Definicoes Contraste de Cores Sass

---
+++ javascript/scripts--materialize.js
@@ -0,0 +1,19 @@
+// BOTAO MENU MOBILE 
+
+$(".button-collapse").sideNav();
+
+// ==================================================
+
+// PUSHPIN FIXAR MENU TOP
+
+$(document).ready(function(){
+    $('.target').pushpin({
+      top: 50,
+      bottom: 1000,
+      offset: 0
+    });
+  });
+
+//   MUDAR SÓ O TOP PARA FUNCIONAR
+
+// ==================================================
